bf1855a3bf40194669b3b53bd70d8d468ba11c9d
LewdHuTao <imanimtiyaz242@gmail.com>
2025-10-31 11:02:32 +0800

    change limit to 20 requests per minute

--- utils/ratelimit.ts
+++ utils/ratelimit.ts
@@ -2,7 +2,7 @@ import rateLimit from "express-rate-limit";
 
 const ratelimit = rateLimit({
     windowMs: 1 * 60 * 1000,
-    max: 100,
+    max: 20,
     message: {
         message: "Too many requests, please try again later.",
         response: "429 Too Many Requests"
